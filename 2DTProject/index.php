48996941496d9a70f6576c7180013fab15282bee
Ollie Hunt <99240295+OllieHunt23@users.noreply.github.com>
2024-08-07 09:21:08 +1200

    gsnsherthjs

--- 2DTProject/index.php
+++ 2DTProject/index.php
@@ -38,7 +38,19 @@ include('includes/carousel.php');
   </div>
 
   <!-- Start of content 2 -->
-
+  <div class="card text-center">
+    <div class="card-header">
+      Featured
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Special title treatment</h5>
+      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    <div class="card-footer text-body-secondary">
+      2 days ago
+    </div>
+  </div>
 
 
 
