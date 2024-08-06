0049e285a3e05a7183280bc03a6f9555fe1f5dab
Ollie Hunt <99240295+OllieHunt23@users.noreply.github.com>
2024-08-07 09:43:29 +1200

    ghnstzghn

--- 2DTProject/contactus.php
+++ 2DTProject/contactus.php
@@ -27,7 +27,7 @@ include('includes/Text-resizer.php');
                 New Zealand <br><br>
                 Telephone: 04 123-4567 <br>
                 Fax: 04 123-4567 <br>
-                Email: <a class="text-colour" href="mailto:WisnewskisCafe@gmail.co.nz">WisnewskiCafe@gmail.co.nz</a><br>
+                Email: <a class="text-colour" href="mailto:Polishman@gmail.co.nz">Polishman@gmail.co.nz</a><br>
             </p><br>
             <h3 class="text-center text-colour">Our Location </h3>
             <iframe class="embed-responsive-item text-center" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5992.732187818479!2d174.80118!3d-41.322652!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d38af89b9179e07%3A0x19b23c8fc92c1dfd!2s170%20Coutts%20Street%2C%20Rongotai%2C%20Wellington%206022!5e0!3m2!1sen!2snz!4v1682890221796!5m2!1sen!2snz" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
