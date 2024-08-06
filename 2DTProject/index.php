48eda0e6e8f1b42063e4ed7d308045444cc585ec
Ollie Hunt <99240295+OllieHunt23@users.noreply.github.com>
2024-08-07 09:25:04 +1200

    gfshth

--- 2DTProject/index.php
+++ 2DTProject/index.php
@@ -47,9 +47,6 @@ include('includes/carousel.php');
       <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
       <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
-    <div class="card-footer text-body-secondary">
-      2 days ago
-    </div>
   </div>
 
 
