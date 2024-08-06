ec0f451dbea553402aabd2c8ed72d54754f7426c
Circlq <edward.wisnewski@my.rongotai.school.nz>
2024-08-07 09:25:46 +1200

    Update nav.php

--- 2DTProject/includes/nav.php
+++ 2DTProject/includes/nav.php
@@ -1,34 +1,40 @@
 <!-- Start of Navbar -->
-<div class="container-fluid" style=background-color:rgb(0,0,0)>
-  <nav class="navbar navbar-expand-lg">
-    <div class="container-fluid text-white">
-      <a class="navbar-brand text-white" href="login.php">Wisnewski's Cafe</a>
-      <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"><i class="fa fa-bars" aria-hidden="true" style="color:#fff"></i></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active text-white" aria-current="page" href="index.php">HOME</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="menu.php">MENU</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="aboutus.php">ABOUT US</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="contactus.php">CONTACT US</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="gallery.php">GALLERY</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="booking.php">BOOK</a>
-          </li>
-        </ul>
-      </div>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
-  </nav>
-</div>
+  </div>
+</nav>
 <!-- End of Navbar -->
