010b281860135715734b406e75ef46156b12b97f
Circlq <edward.wisnewski@my.rongotai.school.nz>
2024-08-15 10:45:53 +1200

    Update nav.php

--- 2DTProject/includes/nav.php
+++ 2DTProject/includes/nav.php
@@ -30,9 +30,9 @@
           <a class="nav-link text-colour" aria-disabled="true">Disabled</a>
         </li>
       </ul>
-      <form class="d-flex button-hover" role="search">
+      <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <button class="btn btn-outline-success button-hover" type="submit">Search</button>
       </form>
     </div>
   </div>
