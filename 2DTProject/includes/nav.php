ebd74a67bd7b40981d0e92fbea362bb61727e4ee
Ollie Hunt <99240295+OllieHunt23@users.noreply.github.com>
2024-08-26 14:32:54 +1200

    Update nav.php

--- 2DTProject/includes/nav.php
+++ 2DTProject/includes/nav.php
@@ -23,7 +23,7 @@
             <li>
               <hr class="dropdown-divider">
             </li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item text-colour" href="#">Something else here</a></li>
           </ul>
         </li>
         <li class="nav-item">
