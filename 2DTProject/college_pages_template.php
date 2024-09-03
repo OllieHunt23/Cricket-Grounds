5364cf8cfcbcdcfd137c87541413275539f7816f
Ollie Hunt <99240295+OllieHunt23@users.noreply.github.com>
2024-09-04 09:49:16 +1200

    Update college_pages_template.php

--- 2DTProject/college_pages_template.php
+++ 2DTProject/college_pages_template.php
@@ -25,6 +25,7 @@ include('includes/nav.php');
                 <h1 class="heading_3 text-center text-colour">Wisnewski's Cafe</h1>
                 <p class="text-center">
                     <img class="img" src="image/logo2.jpg" alt="logo">
+                    <img class="img" src="image/logo2.jpg" alt="logo">
                 </p>
             </div>
 
@@ -42,9 +43,7 @@ include('includes/nav.php');
 
     </div>
     <div class="col-sm-10">
-        <h1 class=" heading_3 text-center text-colour">Contact Us</h1>
 
-        <hr>
 
         <h3 class="text-center text-colour">Our Location </h3>
         <iframe class="embed-responsive-item text-center" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5992.732187818479!2d174.80118!3d-41.322652!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d38af89b9179e07%3A0x19b23c8fc92c1dfd!2s170%20Coutts%20Street%2C%20Rongotai%2C%20Wellington%206022!5e0!3m2!1sen!2snz!4v1682890221796!5m2!1sen!2snz" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
