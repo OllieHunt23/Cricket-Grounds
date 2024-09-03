ff61c20e31c39a45673b37585213db5a1dbb3b74
Circlq <edward.wisnewski@my.rongotai.school.nz>
2024-09-04 09:31:20 +1200

    :)

--- 2DTProject/college_pages_template.php
+++ 2DTProject/college_pages_template.php
@@ -20,26 +20,46 @@ include('includes/carousel.php');
         <div class="row">
 
             <div class="col-sm-1">
-                <div class="col-sm-1">
 
-                </div>
             </div>
+            <div class="col-sm-10">
+                <h1 class="heading_3 text-center text-colour">Wisnewski's Cafe</h1>
+                <p class="text-center">
+                    <img class="img" src="image/logo2.jpg" alt="logo">
+                </p>
 
-
-        </div>
-
-        <!-- Start of content 2 -->
-        <div class="card text-center bg-colour text-colour">
-            <div class="card-header text-colour bg-colour">
             </div>
-            <div class="card-body bg-colour text-colour">
+            <div class="col-sm-1">
 
             </div>
         </div>
 
 
+    </div>
 
-        <!-- Calling footer page-->
-        <?php
-        include 'includes/footer.php';
-        ?>
+</div>
+<div class="col-sm-10">
+    <h1 class=" heading_3 text-center text-colour">Contact Us</h1>
+
+    <hr>
+
+    <h3 class="text-center text-colour">Our Location </h3>
+    <iframe class="embed-responsive-item text-center" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5992.732187818479!2d174.80118!3d-41.322652!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d38af89b9179e07%3A0x19b23c8fc92c1dfd!2s170%20Coutts%20Street%2C%20Rongotai%2C%20Wellington%206022!5e0!3m2!1sen!2snz!4v1682890221796!5m2!1sen!2snz" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <br><br>
+</div>
+
+<!-- Start of content 2 -->
+<div class="card text-center bg-colour text-colour">
+    <div class="card-header text-colour bg-colour">
+    </div>
+    <div class="card-body bg-colour text-colour">
+
+    </div>
+</div>
+
+
+
+<!-- Calling footer page-->
+<?php
+include 'includes/footer.php';
+?>
