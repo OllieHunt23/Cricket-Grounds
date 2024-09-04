79cef36cda959513ae20fdd8705cc6b4f7577a28
Ollie Hunt <99240295+OllieHunt23@users.noreply.github.com>
2024-09-05 10:35:33 +1200

    a

--- 2DTProject/college_pages_template.php
+++ 2DTProject/college_pages_template.php
@@ -39,7 +39,7 @@ include('includes/nav.php');
 
 
         <h3 class="text-center text-colour">Our Location </h3>
-        <iframe class="embed-responsive-item text-center" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5992.732187818479!2d174.80118!3d-41.322652!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d38af89b9179e07%3A0x19b23c8fc92c1dfd!2s170%20Coutts%20Street%2C%20Rongotai%2C%20Wellington%206022!5e0!3m2!1sen!2snz!4v1682890221796!5m2!1sen!2snz" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe class="embed-responsive-item text-center" src="<iframe src=" https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d20291.53819472412!2d174.7706749335901!3d-41.310949763779355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x6d38afdc4c1bcdbf%3A0xf00ef62249daea0!2sBasin%20Reserve%2C%20Mount%20Cook%2C%20Wellington%206021!3m2!1d-41.300366499999996!2d174.7803198!4m5!1s0x6d38af89b90df2ff%3A0x52dc784ada37b6ab!2sRongotai%20College%20Coutts%20Street%2C%20Rongotai%2C%20Wellington!3m2!1d-41.3226286!2d174.8011818!5e1!3m2!1sen!2snz!4v1725489251588!5m2!1sen!2snz" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         <br><br>
     </div>
 
