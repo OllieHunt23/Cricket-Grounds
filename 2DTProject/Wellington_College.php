86ef1331573143bdfa297703f9511ccd61b23ac6
Circlq <edward.wisnewski@my.rongotai.school.nz>
2024-09-09 14:36:21 +1200

    l

---
+++ 2DTProject/Wellington_College.php
@@ -0,0 +1,48 @@
+<!-- Calling header page-->
+<?php
+$page_title = "Finding The Crease";
+include('includes/header.php');
+include('includes/nav.php');
+?>
+
+<!-- Resize controls -->
+<div id="sizecontroldiv" class="controlstyle">
+    <a href="#smaller"><i class="fa fa-minus" aria-hidden="true"></i></a>
+    <a href="#bigger"><i class="fa fa-plus" aria-hidden="true"></i></a>
+</div>
+
+<!-- Start of Content -->
+
+<div class="container">
+    <div class="row justify-content-sm-center">
+        <div class="col col-sm-2">
+            <p class="text-center">
+                <img class="img" src="image/Rongotai.png" alt="logo">
+            </p>
+        </div>
+        <div class="col-sm">
+            <h1 class="heading_3 text-center text-colour">Wellington College</h1>
+        </div>
+        <div class="col col-sm-2">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col col-sm-auto">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d20291.53819472412!2d174.7706749335901!3d-41.310949763779355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x6d38afdc4c1bcdbf%3A0xf00ef62249daea0!2sBasin%20Reserve%2C%20Mount%20Cook%2C%20Wellington%206021!3m2!1d-41.300366499999996!2d174.7803198!4m5!1s0x6d38af89b90df2ff%3A0x52dc784ada37b6ab!2sRongotai%20College%20Coutts%20Street%2C%20Rongotai%2C%20Wellington!3m2!1d-41.3226286!2d174.8011818!5e1!3m2!1sen!2snz!4v1725489382750!5m2!1sen!2snz" width="575" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="col-sm-1">
+
+        </div>
+        <div class="col col-sm-4">
+            <img class="img" src="image/Card 1.jpg" alt="logo">
+            <p class="pt-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid odit velit eligendi obcaecati esse harum, eum atque vel quae quidem a cupiditate officiis quaerat assumenda suscipit illo eaque
+            </p>
+        </div>
+    </div>
+</div>
+
+
+<!-- Calling footer page-->
+<?php
+include 'includes/footer.php';
+?>
