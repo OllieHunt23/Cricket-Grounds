1cdd17dc6fbbbf69a5dbd4aa5da26c7110171a17
Ollie Hunt <99240295+OllieHunt23@users.noreply.github.com>
2024-09-09 14:18:58 +1200

    dgawrhen

--- 2DTProject/index.php
+++ 2DTProject/index.php
@@ -11,38 +11,40 @@ include('includes/carousel.php');
 <!-- Start of content-->
 <div id="sizecontroldiv" class="controlstyle">
 
-  <a href="#smaller"><i class="fa fa-minus" aria-hidden="true"></i></a> <a href="#bigger"><i class="fa fa-plus" aria-hidden="true"></i></a><br><br>
+  <a href="#smaller"><i class="fa fa-minus" aria-hidden="true"></i></a> <a href="#bigger"><i class="fa fa-plus" aria-hidden="true"></i></a>
+</div>
 
 
 
 
-  <div class="container pt-5 controlstyle">
-    <div class="row">
+<div class="container pt-5 controlstyle">
+  <div class="row">
 
+    <div class="col-sm-1">
       <div class="col-sm-1">
-        <div class="col-sm-1">
 
-        </div>
-      </div>
-
-
-    </div>
-
-    <!-- Start of content 2 -->
-    <div class="card text-center bg-colour text-colour">
-      <div class="card-header text-colour bg-colour">
-        Featured
-      </div>
-      <div class="card-body bg-colour text-colour">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text text-colour">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="contactus.php" class="btn btn-primary">Find Schools</a>
       </div>
     </div>
 
 
+  </div>
 
-    <!-- Calling footer page-->
-    <?php
-    include 'includes/footer.php';
-    ?>
+  <!-- Start of content 2 -->
+  <div class="card text-center bg-colour text-colour">
+    <div class="card-header bg-colour text-colour">
+      Featured
+    </div>
+    <div class="card-body bg-colour text-colour">
+      <h5 class="card-title">Special title treatment</h5>
+      <p class="card-text text-colour">With supporting text below as a natural lead-in to additional content.</p>
+      <a href="college_pages_template.php" class="btn btn-primary">Find Schools</a>
+    </div>
+  </div>
+</div>
+
+
+
+<!-- Calling footer page-->
+<?php
+include 'includes/footer.php';
+?>
