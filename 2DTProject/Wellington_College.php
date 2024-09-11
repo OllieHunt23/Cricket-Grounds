87db5dabf524d75dfb9d4b37b4e846543e4576e4
Ollie Hunt <99240295+OllieHunt23@users.noreply.github.com>
2024-09-12 10:49:40 +1200

    a

--- 2DTProject/Wellington_College.php
+++ 2DTProject/Wellington_College.php
@@ -28,14 +28,20 @@ include('includes/nav.php');
     </div>
     <div class="row">
         <div class="col col-sm-auto">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d20291.53819472412!2d174.7706749335901!3d-41.310949763779355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x6d38afdc4c1bcdbf%3A0xf00ef62249daea0!2sBasin%20Reserve%2C%20Mount%20Cook%2C%20Wellington%206021!3m2!1d-41.300366499999996!2d174.7803198!4m5!1s0x6d38af89b90df2ff%3A0x52dc784ada37b6ab!2sRongotai%20College%20Coutts%20Street%2C%20Rongotai%2C%20Wellington!3m2!1d-41.3226286!2d174.8011818!5e1!3m2!1sen!2snz!4v1725489382750!5m2!1sen!2snz" width="575" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d2388.5077091051185!2d174.77785502515778!3d-41.30140112131098!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x6d38afdc4c1bcdbf%3A0xf00ef62249daea0!2sBasin%20Reserve%2C%20Mount%20Cook%2C%20Wellington!3m2!1d-41.300366499999996!2d174.7803198!4m5!1s0x6d38afdda09c31d3%3A0xf188ad3b391f6e8c!2sWellington%20College%2015%20Dufferin%20Street%2C%20Mount%20Victoria%2C%20Wellington%206011!3m2!1d-41.3035277!2d174.7820926!5e1!3m2!1sen!2snz!4v1726094677961!5m2!1sen!2snz" width="575" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
         <div class="col-sm-1">
 
         </div>
         <div class="col col-sm-4">
-            <img class="img" src="image/Card 1.jpg" alt="logo">
-            <p class="pt-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid odit velit eligendi obcaecati esse harum, eum atque vel quae quidem a cupiditate officiis quaerat assumenda suscipit illo eaque
+            <img class="img" src="image/WC_location.jpeg" alt="logo">
+            <h5 class="heading_3 text-center text-colour pt-5">Info:</h5>
+            <p class=" text-colour">
+            <ul>
+                <li class="text-colour">Professional Coaches</li>
+                <li class="text-colour">Developmental Programmes</li>
+                <li class="text-colour">Ryan Tsourgas and Will Julian 1st XI 2023 were selected for the NZ U 19 cricket team.</li>
+            </ul>
             </p>
         </div>
     </div>
