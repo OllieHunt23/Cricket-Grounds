005b17dab689b5244a09f93cc5dec78f08479074
Ollie Hunt <99240295+OllieHunt23@users.noreply.github.com>
2024-09-11 19:04:09 +1200

    a

---
+++ 2DTProject/HIBS.php
@@ -0,0 +1,49 @@
+<!-- Calling header page-->
+<?php
+$page_title = "Finding The Crease";
+include('includes/header.php');
+include('includes/nav.php');
+?>
+
+<!-- Resize controls -->
+<div id="sizecontroldiv" class="controlstyle">
+    <a href="#smaller"><i class="fa fa-minus" aria-hidden="true"></i></a>
+    <a href="#bigger"><i class="fa fa-plus" aria-hidden="true"></i></a>
+</div>
+
+<!-- Start of Content -->
+
+<div class="container">
+    <div class="row justify-content-sm-center">
+        <div class="col col-sm-2">
+            <p class="text-center">
+                <img class="img" src="image/hibs_logo.png" alt="logo">
+            </p>
+        </div>
+        <div class="col-sm">
+            <h1 class="heading_3 text-center text-colour">HIBS</h1>
+        </div>
+        <div class="col col-sm-2">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col col-sm-auto">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d4280.126497441844!2d175.03550454798614!3d-41.13906726858376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x6d38afdc4c1bcdbf%3A0xf00ef62249daea0!2sBasin%20Reserve%2C%20Mount%20Cook%2C%20Wellington%206021!3m2!1d-41.300366499999996!2d174.7803198!4m5!1s0x6d40ac8f3a5739e1%3A0x35685428fe334fd9!2sHutt%20International%20Boys&#39;%20School%20Granville%20Street%2C%20Trentham%2C%20Upper%20Hutt!3m2!1d-41.1406831!2d175.0367607!5e1!3m2!1sen!2snz!4v1726037979697!5m2!1sen!2snz" width="575" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="col-sm-1">
+
+        </div>
+        <div class="col col-sm-4">
+            <img class="img" src="image/HIBS_Location.png" alt="logo">
+            <h5 class="heading_3 text-center text-colour">Facts:</h5>
+            <p class="pt-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid odit velit eligendi obcaecati esse harum, eum atque vel quae quidem a cupiditate officiis quaerat assumenda suscipit illo eaque
+            </p>
+        </div>
+    </div>
+</div>
+
+
+<!-- Calling footer page-->
+<?php
+include 'includes/footer.php';
+?>
