48cea719ca56fb6e759fe3a4f187256ddab23ea8
Ollie Hunt <99240295+OllieHunt23@users.noreply.github.com>
2024-09-11 20:03:36 +1200

    a

--- 2DTProject/find_schools.php
+++ 2DTProject/find_schools.php
@@ -25,7 +25,7 @@ include('includes/nav.php');
                 <div class="card" style="width: 18rem;">
                     <img src="image/Card 1.jpg" class="card-img-top" alt="Rongotai">
                     <div class="card-body bg-colour">
-                        <h5 class="text-colour">Rongotai</h5>
+                        <h5 class="text-colour pb-1">Rongotai</h5>
                         <a href="Rongotai.php" class="button">Rongotai College info</a>
                     </div>
                 </div>
@@ -34,7 +34,7 @@ include('includes/nav.php');
                 <div class="card" style="width: 18rem;">
                     <img src="" class="card-img-top" alt="WC">
                     <div class="card-body bg-colour">
-                        <h5 class="text-colour">Wellington College</h5>
+                        <h5 class="text-colour pb-1">Wellington College</h5>
                         <a href="Wellington_College.php" class="button button:hover">Wellington College info</a>
                     </div>
                 </div>
@@ -43,7 +43,7 @@ include('includes/nav.php');
                 <div class="card" style="width: 18rem;">
                     <img src="image/pats_location.png" class="card-img-top" alt="Pats">
                     <div class="card-body bg-colour">
-                        <h5 class="text-colour">St. Pats Town</h5>
+                        <h5 class="text-colour pb-1">St. Pats Town</h5>
                         <a href="Pats.php" class=" button button:hover">St. Pats info</a>
                     </div>
                 </div>
@@ -52,9 +52,9 @@ include('includes/nav.php');
         <div class="row pb-5">
             <div class="col-sm-4">
                 <div class="card" style="width: 18rem;">
-                    <img src="image/scots_logo.png" class="card-img-top" alt="Scots">
+                    <img src="image/scots_location.png" class="card-img-top" alt="Scots">
                     <div class="card-body bg-colour">
-                        <h5 class="text-colour">Scots College</h5>
+                        <h5 class="text-colour pb-1">Scots College</h5>
                         <a href="Scots_College.php" class="button">Scots College info</a>
                     </div>
                 </div>
@@ -63,7 +63,7 @@ include('includes/nav.php');
                 <div class="card" style="width: 18rem;">
                     <img src="image/stream_location.png" class="card-img-top" alt="stream">
                     <div class="card-body bg-colour">
-                        <h5 class="text-colour">St. Pats Stream</h5>
+                        <h5 class="text-colour pb-1">St. Pats Stream</h5>
                         <a href="Stream.php" class="button button:hover">St. Pats Stream info</a>
                     </div>
                 </div>
@@ -72,7 +72,7 @@ include('includes/nav.php');
                 <div class="card" style="width: 18rem;">
                     <img src="image/HVHS_Location.png" class="card-img-top" alt="HVHS">
                     <div class="card-body bg-colour">
-                        <h5 class="text-colour">HVHS</h5>
+                        <h5 class="text-colour pb-1">HVHS</h5>
                         <a href="HVHS.php" class=" button button:hover">HVHS info</a>
                     </div>
                 </div>
@@ -83,7 +83,7 @@ include('includes/nav.php');
                 <div class="card" style="width: 18rem;">
                     <img src="image/HIBS_Location.png" class="card-img-top" alt="HIBS">
                     <div class="card-body bg-colour">
-                        <h5 class="text-colour">HIBS</h5>
+                        <h5 class="text-colour pb-1">HIBS</h5>
                         <a href="HIBS.php" class="button">HIBS info</a>
                     </div>
                 </div>
@@ -92,7 +92,7 @@ include('includes/nav.php');
                 <div class="card" style="width: 18rem;">
                     <img src="image/Onslow_location.png" class="card-img-top" alt="Onslow">
                     <div class="card-body bg-colour">
-                        <h5 class="text-colour">Onslow College</h5>
+                        <h5 class="text-colour pb-1">Onslow College</h5>
                         <a href="Onslow.php" class="button button:hover">Onslow College info</a>
                     </div>
                 </div>
