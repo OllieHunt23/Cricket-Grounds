3a636c072a5fd7b22158c5a8f91329cbc20ad9c9
Ollie Hunt <99240295+OllieHunt23@users.noreply.github.com>
2024-09-13 11:43:07 +1200

    a

--- 2DTProject/includes/nav.php
+++ 2DTProject/includes/nav.php
@@ -1,7 +1,7 @@
 <!-- Start of Navbar -->
 <nav class="navbar navbar-expand-lg bg-colour navbar">
   <div class="container-fluid">
-    <a class="navbar-brand text-colour" href="#">Navbar</a>
+    <a class="navbar-brand text-colour" href="login.php">Finding the Crease</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
